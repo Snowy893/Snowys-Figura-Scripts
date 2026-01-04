a299d6ace92f89913e791c4e6b9f450822898513
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-01-04 04:10:13 -0500

    Added example for SyncedPings

---
+++ SyncedPings/example.lua
@@ -0,0 +1,20 @@
+local syncedPings = require "syncedpings"
+
+local page = action_wheel:newPage()
+
+action_wheel:setPage(page)
+
+function pings.myPing(state)
+    log(state)
+end
+
+local mySyncedPing = syncedPings:new(pings.myPing, true)
+
+page:newAction()
+    :title("Action")
+    :onToggle(mySyncedPing)
+
+-- passing it directly is also an option!
+page:newAction()
+    :title("Action")
+    :onToggle(syncedPings:new(pings.myPing, true))
