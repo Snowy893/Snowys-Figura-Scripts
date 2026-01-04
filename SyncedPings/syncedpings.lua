e13efa335b873ce90c3041a82fb9e0d4a8a6fa8b
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-01-04 04:05:03 -0500

    Synced Pings Initial Commit
    Changed iterator from `pairs` to `ipairs`

---
+++ SyncedPings/syncedpings.lua
@@ -0,0 +1,29 @@
+---@class SyncedPings
+local SyncedPings = {}
+---@type SyncedPings[]
+SyncedPings.ALL = {}
+
+SyncedPings.ticks = 200
+
+---@param pingFunc function
+---@param ... any
+---@return function
+function SyncedPings:new(pingFunc, ...)
+    self.pingFunc = pingFunc
+    self.args = ...
+    table.insert(SyncedPings.ALL, self)
+    return function (...)
+        self.args = ...
+        self.pingFunc(...)
+    end
+end
+
+events.TICK:register(function ()
+    for i, sPing in ipairs(SyncedPings.ALL) do
+        if world.getTime() % (SyncedPings.ticks + i) == 0 then
+            sPing.pingFunc(sPing.args)
+        end
+    end
+end, "SyncedPings")
+
+return SyncedPings
