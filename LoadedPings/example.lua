6674c103470e7944a46043e5f959c299c5b4c8fb
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-01-07 05:28:56 -0500

    Added example for LoadedPings

---
+++ LoadedPings/example.lua
@@ -0,0 +1,11 @@
+function loadedPings.squeak()
+    sounds:playSound("minecraft:entity.bat.hurt", player:getPos())
+end
+
+local page = action_wheel:newPage()
+
+action_wheel:setPage(page)
+
+page:newAction()
+    :title("Action")
+    :onLeftClick(loadedPings.squeak)
