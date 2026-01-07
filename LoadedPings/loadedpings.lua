f81d6ccc7838c0d25e6edec0c6467fed851e946f
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-01-07 05:28:44 -0500

    Loaded Pings Initial Commit

---
+++ LoadedPings/loadedpings.lua
@@ -0,0 +1,18 @@
+---@class LoadedPings
+local loadedPings = {}
+local _loadedPings = {}
+
+setmetatable(loadedPings, {
+    __index = _loadedPings,
+    __newindex = function(_, index, value)
+        pings[index] = value
+        local function func(...)
+            if player:isLoaded() then
+                pings[index](...)
+            end
+        end
+        tbl[index] = func
+    end
+})
+
+rawset(_G, "loadedPings", loadedPings)
