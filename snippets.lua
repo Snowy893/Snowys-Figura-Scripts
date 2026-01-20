2c4cb6ea5ce2401ee38b1db3926b94838cd7ae60
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-01-20 11:39:03 -0500

    Initial Commit of snippets
    - Added my snippet for offsetting your arm rotation when holding a charged crossbow while crouching

---
+++ snippets.lua
@@ -0,0 +1,24 @@
+-----------------------------------------------------------------------------
+
+---Offset charged crossbow crouching rotation (fix when the crossbow points downward when you crouch!)
+
+---@param itemStack ItemStack
+---@return boolean
+local function crossbowCharged(itemStack)
+    local projectiles = itemStack:getTag().ChargedProjectiles
+    return projectiles ~= nil and next(projectiles) ~= nil
+end
+
+function events.tick()
+    local leftHanded = player:isLeftHanded()
+    local rightItem = player:getHeldItem(leftHanded)
+    local leftItem = player:getHeldItem(not leftHanded)
+    local crouching = player:isCrouching()
+    local charged = crossbowCharged(rightItem) or crossbowCharged(leftItem)
+
+    local armRot = (crouching and charged) and 20 or nil
+    vanilla_model.RIGHT_ARM:setOffsetRot(armRot)
+    vanilla_model.LEFT_ARM:setOffsetRot(armRot)
+end
+
+-----------------------------------------------------------------------------
