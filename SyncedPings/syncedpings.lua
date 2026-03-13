d3dc352f081e2057fa7f7616ba1b720b7d849e1c
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-03-13 01:04:57 -0400

    Updated Synced Pings to use a better timer I think

--- SyncedPings/syncedpings.lua
+++ SyncedPings/syncedpings.lua
@@ -3,7 +3,6 @@ local SyncedPings = {}
 SyncedPings.ticks = 200
 
 local objs = {}
-local timer = 0
 
 ---@generic T
 ---@param pingFunc fun(...: T?)
@@ -13,6 +12,7 @@ function SyncedPings:new(pingFunc, ...)
     if not host:isHost() then return function() end end
     self.pingFunc = pingFunc
     self.args = ...
+    self.timer = 0
     table.insert(objs, self)
     return function(...)
         self.args = ...
@@ -21,10 +21,11 @@ function SyncedPings:new(pingFunc, ...)
 end
 
 events.TICK:register(function()
-    timer = timer + 1
-    for i, sPing in ipairs(objs) do
-        if timer % (SyncedPings.ticks + (i - 1)) == 0 then
-            sPing.pingFunc(sPing.args)
+    for i, obj in ipairs(objs) do
+        obj.timer = obj.timer + 1
+        if obj.timer == SyncedPings.ticks + (i - 1) == 0 then
+            obj.timer = 0
+            obj.pingFunc(obj.args)
         end
     end
 end, "SyncedPings")
